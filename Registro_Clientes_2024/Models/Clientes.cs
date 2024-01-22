f5423f20fbec0c5b50750e5bfc60403997b2e23e
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-21 21:03:03 -0400

    Creamos nuestro modelo Clientes

---
+++ Registro_Clientes_2024/Models/Clientes.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Registro_Clientes_2024.Models
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        public string? Nombres { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int Telefono { get; set; }
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int Celular { get;}
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public int Rnc { get; set;}
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Email { get; set;}
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        public string? Direccion { get; set;}
+
+        //ClienteId, Nombres, Telefono, Celular, Rnc, Email, Direccion
+    }
+}
