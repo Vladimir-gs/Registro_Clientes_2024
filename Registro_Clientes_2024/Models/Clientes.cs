e363371f2027d524654180c96f192cec9f428e98
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-22 13:53:23 -0400

    Agregamos algunas validaciones

--- Registro_Clientes_2024/Models/Clientes.cs
+++ Registro_Clientes_2024/Models/Clientes.cs
@@ -9,8 +9,12 @@ namespace Registro_Clientes_2024.Models
         [Required(ErrorMessage ="Este campo es obligatorio")]
         public string? Nombres { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(10, ErrorMessage = "El número de teléfono no puede tener más de 10 dígitos")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "El número de teléfono solo puede contener dígitos")]
         public string? Telefono { get; set; }
         [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MaxLength(9, ErrorMessage = "El número de teléfono no puede tener más de 10 dígitos")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "El número de teléfono solo puede contener dígitos")]
         public string? Rnc { get; set;}
         [Required(ErrorMessage = "Este campo es obligatorio")]
         public string? Email { get; set;}
