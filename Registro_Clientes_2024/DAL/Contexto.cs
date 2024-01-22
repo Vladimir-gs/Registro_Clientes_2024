772d0b7ce8fd48f0c6751e75f82f9e84150c04f7
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2024-01-21 21:06:17 -0400

    Creamos Nuestro Contexto

---
+++ Registro_Clientes_2024/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Registro_Clientes_2024.Models;
+
+namespace Registro_Clientes_2024.DAL
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Clientes> Clientes { get; set; }
+
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+
+    }
+}
